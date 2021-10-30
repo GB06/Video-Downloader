b4449619fdfa3d5ac1bf1ffeaeb6fa9ec3fb35f6
Gabriel Tsen <87059445+GB06@users.noreply.github.com>
2021-10-30 17:06:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pafy
 import youtube_dl
 
-link = "https://youtu.be/b80a8XKX6ws" # link must be in ""youtube.be"
+link = "https://youtu.be/b80a8XKX6ws" # link must be in "youtube.be"
 video = pafy.new(link)
 best = video.getbest()
 print(best.resolution, best.extension)
